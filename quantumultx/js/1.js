99916a6fe6d3d6cd81411581b14dedf78147e246
listacc1sa <166372012+listacc1sa@users.noreply.github.com>
2024-12-17 12:25:52 +0300

    Create 1.js

---
+++ quantumultx/js/1.js
@@ -0,0 +1,32 @@
+/*
+rochat
+
+[rewrite_local]
+https://api.rochathub.com/inteco.user.v1.UserRightsService/GetUserRights url script-response-body https://github.com/listacc1sa/Ss/raw/refs/heads/main/quantumultx/js/1.js
+
+[mitm]
+hostname = api.rochathub.com
+
+*/
+
+var objc = JSON.parse($response.body);
+    objc = {
+  "rights" : {
+    "reachedAppRating" : true,
+    "memberShipRights" : {
+      "term" : "GIVEAWAY",
+      "expiredtime" : "1883639265",
+      "gameCredit" : 10,
+      "userRoleType" : "CREATOR",
+      "starttime" : "1734391800"
+    },
+    "generabotDisplayName" : "General Bot",
+    "premiumbotDisplayName" : "Premium Bot",
+    "isInvited" : true,
+    "gptDisplayName" : "GPT-4"
+  }
+}
+}
+
+
+$done({body : JSON.stringify(objc)});
