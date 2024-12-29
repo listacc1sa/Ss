f26e891db433f66b7d1db57feff6152d31223279
listacc1sa <166372012+listacc1sa@users.noreply.github.com>
2024-12-30 01:27:15 +0300

    Update AllinOne.js

--- quantumultx/conf/AllinOne.js
+++ quantumultx/conf/AllinOne.js
@@ -6,8 +6,9 @@ auto = type=http-response, pattern=^https?:\/\/api\.revenuecat\.com\/.+\/(receip
 
 apphud = type=http-response, pattern=https:\/\/api\.apphud\.com\/v1\/(subscriptions|customers), script-path=https://raw.githubusercontent.com/listacc1sa/Ss/refs/heads/main/quantumultx/js/10mail.js, requires-body=true, script-update-interval=0, timeout=10
 
-
 chat Uncensored = type=http-response, pattern=https://api.adapty.io/api/v1/sdk/in-apps/apple/receipt/validate/, script-path=https://raw.githubusercontent.com/listacc1sa/Ss/refs/heads/main/quantumultx/js/chatUncensored.js, requires-body=true, script-update-interval=0, timeout=10
+
 chat Uncensored = type=http-response, pattern=https://api.adapty.io/api/v1/sdk/analytics/profiles, script-path=https://raw.githubusercontent.com/listacc1sa/Ss/refs/heads/main/quantumultx/js/chatUncensored.js, requires-body=true, script-update-interval=0, timeout=10
+
 [MITM]
 hostname = %APPEND% api.adapty.io, api.apphud.com, api.revenuecat.com
